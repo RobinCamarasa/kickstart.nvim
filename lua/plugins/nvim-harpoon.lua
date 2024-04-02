c434290f74517147198518d7d85a059fc9ba3131
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-02 09:50:19 +0200

    Add which-key support for harpoon

--- lua/plugins/nvim-harpoon.lua
+++ lua/plugins/nvim-harpoon.lua
@@ -5,11 +5,14 @@ return {
   config = function()
     local harpoon = require 'harpoon'
     harpoon:setup()
+    require('which-key').register {
+      ['<leader>h'] = { name = '[h]arpoon', _ = 'which_key_ignore' },
+    }
     vim.keymap.set('n', '<leader>ha', function()
       harpoon:list():append()
-    end)
+    end, { desc = '[h]arpoon [a]dd' })
     vim.keymap.set('n', '<leader>hq', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = '[h]arpoon [q]uickfix' })
   end,
 }
