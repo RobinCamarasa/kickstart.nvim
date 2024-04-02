401ef514a48e9ba02b7dbee1db43a8f8f418066b
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-02 08:51:24 +0200

    Remove oil useless options

--- lua/plugins/nvim-oil.lua
+++ lua/plugins/nvim-oil.lua
@@ -1,7 +1,5 @@
 return {
   'stevearc/oil.nvim',
-  default_file_explorer = true,
-  skip_confirm_for_simple_edits = false,
   opts = {},
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
