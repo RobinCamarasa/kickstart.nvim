a1806962e6790312876ecb746e9e4481b4c609ce
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-02 09:00:59 +0200

    Add gitsigns shortcuts for staging and unstaging hunks

--- lua/plugins/nvim-gitsigns.lua
+++ lua/plugins/nvim-gitsigns.lua
@@ -3,5 +3,7 @@ return { -- Usefull to show what has been changed since last commit
   config = function()
     require('gitsigns').setup()
     vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { desc = '[g]it [p]review hunk' })
+    vim.keymap.set('n', '<leader>gs', require('gitsigns').stage_hunk, { desc = '[g]it [s]tage hunk' })
+    vim.keymap.set('n', '<leader>gu', require('gitsigns').undo_stage_hunk, { desc = '[g]it [u]ndo stage hunk' })
   end,
 }
