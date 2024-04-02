327775abb45b2c710cf34b4a5695ecb8b1bd0fdb
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-02 08:52:41 +0200

    Improve git signs support

--- lua/plugins/nvim-gitsigns.lua
+++ lua/plugins/nvim-gitsigns.lua
@@ -1,12 +1,7 @@
 return { -- Usefull to show what has been changed since last commit
   'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+  config = function()
+    require('gitsigns').setup()
+    vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { desc = '[g]it [p]review hunk' })
+  end,
 }
