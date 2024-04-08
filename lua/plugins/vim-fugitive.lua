ad38d9b78a8f2ca6dd1a50bdc8c1331f0e4e3981
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-08 12:06:21 +0200

    Add git commit keyboard shortcut

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,1 +1,7 @@
-return { 'tpope/vim-fugitive', opts = {}, config = function() end }
+return {
+  'tpope/vim-fugitive',
+  opts = {},
+  config = function()
+    vim.keymap.set('n', '<leader>gc', '<CMD>Git commit<CR>', { desc = '[g]it [c]ommit' })
+  end,
+}
