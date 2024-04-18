5cae1179aba0e87ceae91e4483f748f5f1177f47
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-18 15:17:36 +0200

    Add git add

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -3,5 +3,6 @@ return {
   opts = {},
   config = function()
     vim.keymap.set('n', '<leader>gc', '<CMD>Git commit<CR>', { desc = '[g]it [c]ommit' })
+    vim.keymap.set('n', '<leader>ga', '<CMD>Git add %<CR>', { desc = '[g]it [a]dd current buffer' })
   end,
 }
