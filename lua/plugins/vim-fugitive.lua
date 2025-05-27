e7cb826e290cc83e2e2e54cb266c119d7a06ae46
Robin Camarasa <robin.camarasa@tno.nl>
2025-05-27 12:21:01 +0200

    Add git which-key help

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -2,6 +2,9 @@ return {
   'tpope/vim-fugitive',
   opts = {},
   config = function()
+    require('which-key').add {
+      { '<leader>g', group = '[g]it' },
+    }
     vim.keymap.set('n', '<leader>gc', '<CMD>Git commit<CR>', { desc = '[g]it [c]ommit' })
     vim.keymap.set('n', '<leader>ga', '<CMD>Git add %<CR>', { desc = '[g]it [a]dd current buffer' })
   end,
