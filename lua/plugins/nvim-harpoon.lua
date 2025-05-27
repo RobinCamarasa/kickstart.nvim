cbaf5ad123d7f4f093d52558091ecb5de8768769
Robin Camarasa <robin.camarasa@tno.nl>
2025-05-27 12:55:26 +0200

    Add new shortcuts for harpoon

--- lua/plugins/nvim-harpoon.lua
+++ lua/plugins/nvim-harpoon.lua
@@ -9,10 +9,22 @@ return {
       { '<leader>h', group = '[h]arpoon' },
     }
     vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():append()
+      harpoon:list():add()
     end, { desc = '[h]arpoon [a]dd' })
     vim.keymap.set('n', '<leader>hq', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = '[h]arpoon [q]uickfix' })
+    vim.keymap.set('n', '<leader>hh', function()
+      harpoon:list():select(1)
+    end, { desc = '[h]arpoon ([h]) access first file' })
+    vim.keymap.set('n', '<leader>hj', function()
+      harpoon:list():select(2)
+    end, { desc = '[h]arpoon ([j]) access second file' })
+    vim.keymap.set('n', '<leader>hk', function()
+      harpoon:list():select(3)
+    end, { desc = '[h]arpoon ([k]) access third file' })
+    vim.keymap.set('n', '<leader>hl', function()
+      harpoon:list():select(4)
+    end, { desc = '[h]arpoon ([l]) access fourth file' })
   end,
 }
